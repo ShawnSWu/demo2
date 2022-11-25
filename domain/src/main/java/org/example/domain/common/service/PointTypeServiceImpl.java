612b9a9959729fb3354fa31c6e50352221ce5592
Shawn Wu <swshawnwu@gmail.com>
2022-11-25 15:52:01 +0800

    實作domain

---
+++ domain/src/main/java/org/example/domain/common/service/PointTypeServiceImpl.java
@@ -0,0 +1,26 @@
+package org.example.domain.common.service;
+
+import org.example.domain.common.PointType;
+import org.example.domain.common.repository.PointTypeRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public class PointTypeServiceImpl implements PointTypeService{
+
+    private PointTypeRepository repository;
+
+    public PointTypeServiceImpl(PointTypeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UUID createPointType(PointType type) {
+        return repository.save(type);
+    }
+
+    @Override
+    public List<PointType> allTypes() {
+        return repository.getAll();
+    }
+}
